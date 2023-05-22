92311223082a958cbb181bb5d28ea08715da137c
whou <whou@192.168.71.105>
2023-05-22 20:41:42 +0800

    init project

---
+++ pkg/hostpath/utils.go
@@ -0,0 +1,31 @@
+package hostpath
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"google.golang.org/grpc"
+	"k8s.io/klog/v2"
+)
+
+func parseEndpoint(ep string) (string, string, error) {
+	if strings.HasPrefix(strings.ToLower(ep), "unix://") {
+		s := strings.SplitN(ep, "://", 2)
+		if s[1] != "" {
+			return s[0], s[1], nil
+		}
+	}
+	return "", "", fmt.Errorf("invalid endpoint: %v", ep)
+}
+
+func logGRPC(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	klog.V(2).Infof("GRPC call: %s", info.FullMethod)
+
+	resp, err := handler(ctx, req)
+	if err != nil {
+		klog.Errorf("GRPC error: %v", err)
+	}
+
+	return resp, err
+}
